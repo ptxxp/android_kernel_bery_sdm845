c4acdd5fa7fae1b3c1a1867cc3a80306f7e71918
KanonifyX <kanonify01@gmail.com>
2025-04-03 11:51:27 +0000

    sched/uclamp: Tune uclamp assist for better jitter
    Signed-off-by: KanonifyX <kanonify01@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2035,10 +2035,12 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 1},
-		{"foreground", 	     	"0",  "50",  1, 1},
-		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
+		{"top-app",    	     	"30", "max", 1, 1},
+		{"foreground", 	     	"0", "50",   1, 1},
+		{"restricted", 	     	"0", "40",   0, 0},
+		{"background", 	     	"0", "max",  0, 0},
+		{"system-background", 	"0", "40",   0, 0},
+		{"camera-daemon",       "0", "40",   0, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
@@ -2060,7 +2062,7 @@ static void uclamp_set(struct kernfs_open_file *of,
 
 	/* Set systemwide uclamps */
 	sysctl_sched_uclamp_util_min = 128;
-	sysctl_sched_uclamp_util_min_rt_default = 500;
+	sysctl_sched_uclamp_util_min_rt_default = 0;
 }
 #endif
 
